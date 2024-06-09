9a16617a406a8933b59f301d1dea0b61ad833df1
Aga Rizqulloh <2377anr@gmail.com>
2024-06-09 09:34:38 +0700

    add: cabang

--- resources/views/v1/frontend/pages/branch.blade.php
+++ resources/views/v1/frontend/pages/branch.blade.php
@@ -1,11 +1,207 @@
 @extends('v1.frontend.layout.main')
 
-@section('title', 'Kantor Cabang')
+@push('css')
+<style>
+.image-background {
+    background-image: url({{asset('frontend/img/bg-nav.jpg')}});
+    background-size: cover;
+    background-position: center;
+    text-align: center;
+}
+.image-backgroundfoot {
+    background-image: url({{asset('frontend/img/bg-nav2.jpg')}});
+    background-size: cover;
+    background-position: center;
+}
+.image-backgroundfooter {
+    background-image: url({{asset('frontend/img/bg-nav.jpg')}});
+    background-size: cover;
+    background-position: center;
+}
+.breadcrumb {
+    padding-bottom: 200px;
+    background: none;
+    padding: 0;
+    justify-content: center; 
+}
+.breadcrumb-item + .breadcrumb-item::before {
+    color: white;
+}
+.breadcrumb-item a {
+    color: lightblue;
+}
+.spacing-1{
+    letter-spacing: 1px;
+}
+@media (min-width: 200px) { 
+    .t-desc {
+        font-size: 13px;
+    }
+    .title{
+        padding-top: 3rem;
+        margin-top:0rem; 
+    }
+    .ctr-urutkan{
+        padding-top: 18px !important;
+    }
+}
+
+
+@media (min-width: 319px) { 
+    .t-desc {
+        font-size: 13px;
+    }
+    .title{
+        padding-top: 3rem;
+        margin-top:1rem; 
+    }
+    .ctr-urutkan{
+        padding-top: 18px !important;
+    }
+}
+
+@media (min-width: 374px) { 
+    .t-desc {
+        font-size: 13px; 
+    }
+    .title{
+        padding-top: 3rem;
+        margin-top:1rem; 
+    }
+    .ctr-urutkan{
+        padding-top: 18px !important;
+    }
+}
+
+@media (min-width: 425px) { 
+    .t-desc {
+        font-size: 13px; 
+    }
+    .title{
+        padding-top: 3rem;
+        margin-top:1rem; 
+    }
+    .ctr-urutkan{
+        padding-top: 18px !important;
+    }
+}
+@media (min-width: 767px) { 
+    .t-desc {
+        font-size: 14px;
+    }
+    .title{
+        padding-top: 3rem;
+        margin-top:1rem; 
+    }
+}
+@media (min-width: 991px) { 
+    .t-desc {
+        font-size: 15px;
+    }
+    .title{
+        padding-top: 3rem;
+        margin-top:3rem; 
+    }
+}
+
+@media (min-width: 1023px) { 
+    .t-desc {
+        font-size: 16px;
+    }
+    .title{
+        padding-top: 3rem;
+        margin-top:3rem; 
+    }
+}
+
+@media (min-width: 1200px) { 
+    .t-desc {
+        font-size: 20px; 
+    }
+    .title{
+        padding-top: 3rem;
+        margin-top:3rem; 
+    }
+}
+</style>
+@endpush
 
 @section('content')
-<h1>Kantor Cabang</h1>
+
+{{-- content header --}}
+<div class="image-background p-3">
+    <h3 class="text-white poppins-semibold title">Toko Cabang</h3>
+    <p class="text-white poppins-regular t-desc spacing-1 mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor <br> incididunt ut labore et dolore magna aliqua.
+    </p>
+    <nav aria-label="breadcrumb mt-2">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item poppins-light"><a href="#">Beranda</a></li>
+            <li class="breadcrumb-item poppins-medium active text-white" aria-current="page">Toko Cabang</li>
+        </ol>
+    </nav>
+</div>
+
+{{-- content body --}}
+<div class="container my-4">
+    <div class="card border-1 shadow rounded-3">
+        <div class="row g-0">
+            <div class="col-md-4 d-flex justify-content-between align-items-center">
+                <div class="p-2">
+                    <img class="img-fluid rounded-3" width="450px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/M%C3%BCnster%2C_LVM%2C_B%C3%BCrogeb%C3%A4ude_--_2013_--_5149-51.jpg/1200px-M%C3%BCnster%2C_LVM%2C_B%C3%BCrogeb%C3%A4ude_--_2013_--_5149-51.jpg" alt="Lima Waktu - Waru">
+                </div>
+            </div>
+            <!-- Content Column -->
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h3 class="card-title poppins-semibold">LIMA WAKTU - WARU</h3>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="" style="width: 700px">
+                            <p class="poppins-regular">POM, Jl. Brigjend Katamso No.37-39, Mekar Raya Binangun, Janti, Kec. Waru, SIDOARJO ( BARAT, Jawa Timur 61256)</p>
+                        </div>
+                        <img class="p-2" width="80px" src="{{asset('frontend/img/icon/iconwa.png')}}" alt="">
+                    </div>
+                    <!-- Map Embed -->
+                    <div class="embed-responsive embed-responsive-16by9 ">
+                        <iframe class="w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.0153790414093!2d-122.08424968529271!3d37.42199977982113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb5b5eacb96d3%3A0x2a6f263ad0b08b8!2sGoogleplex!5e0!3m2!1sen!2sus!4v1641393289245!5m2!1sen!2sus" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="image-backgroundfoot p-3">
+    <div class="row">
+        <div class="col text-center">
+            <img src="{{asset('frontend/img/hubme.png')}}" alt="">
+        </div>
+        <div class="col">
+            <div class="pt-5 mt-5"></div>
+            <h3 class="text-white mt-5 pt-5">Masih ragu dan bingung dengan layanan kami ?</h3>
+            <p class="text-white">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <button class="btn btn-primary">Hubungi Kami</button>
+        </div>
+    </div>
+</div>
+<div class="container pt-4 pb-3">
+    <div class="mx-auto">
+        <div class="d-flex justify-content-between">
+            <img class="mt-2" width="80px" height="35px" src="{{asset('frontend/img/pay/ipay.png')}}" alt="">
+            <img width="120px" height="60px" src="{{asset('frontend/img/pay/gpay.png')}}" alt="">
+            <img class="mt-2" width="70px" height="40px" src="{{asset('frontend/img/pay/samsungpay.png')}}" alt="">
+            <img class="mt-0" width="100px" height="70px" src="{{asset('frontend/img/pay/alipay.png')}}" alt="">
+            <img class="mt-2" width="70px" height="50px" src="{{asset('frontend/img/pay/visa.png')}}" alt="">
+            <img class="mt-2" width="70px" height="40px" src="{{asset('frontend/img/pay/maestro.png')}}" alt="">
+            <img class="mt-2" width="70px" height="40px" src="{{asset('frontend/img/pay/mastercard.png')}}" alt="">
+            <img class="mt-2" width="70px" height="40px" src="{{asset('frontend/img/pay/cirrus.png')}}" alt="">
+        </div>
+    </div>
+</div>
 @endsection
-
-@push('scripts')
-
+@push('script')
+<script src="https://kit.fontawesome.com/d911015868.js" crossorigin="anonymous"></script>
 @endpush
